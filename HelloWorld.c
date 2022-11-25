7e09fe704537152e11a7f0ad89fcf155c51af670
A-Tech-Services <abiodunoluwapelumi859@gmail.com>
2022-11-25 18:04:51 +0100

    new work

---
+++ HelloWorld.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+    printf("Hello World!\n");
+
+    printf("This isn't my first C programming language anyways.");
+
+    printf("\n1\t2\t3\n4\t5\t6");
+    
+
+    return 0;
+}
