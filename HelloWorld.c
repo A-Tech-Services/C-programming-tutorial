96206980984de46fc64eb41eafa33c296f92449e
A-Tech-Services <abiodunoluwapelumi859@gmail.com>
2022-11-25 19:33:03 +0100

    update

--- HelloWorld.c
+++ HelloWorld.c
@@ -5,7 +5,29 @@ int main(){
 
     printf("This isn't my first C programming language anyways.");
 
-    printf("\n1\t2\t3\n4\t5\t6");
+    printf("\n1\t2\t3\n4\t5\t6\n");
+
+    printf("\"I like pizza\" - Abraham Lincoln Probably\n");
+
+    // Declaring a variable and assigning values
+    int age; // variable declared
+    age = 2; // variable initialized
+    float GP;
+
+    GP = 3.07;
+
+    char grade = 'C';
+
+    char name[] = "Oluwapelumi";
+
+    printf("%s\n", name);
+    printf("You are %d Years Old\n", age);
+    printf("Your GP is %0.15f\n", GP);
+    printf("Your Grade is %c\n", grade);
+
+    float CGPA = 2.354;
+
+    printf("your CGPA is: %0.3f", CGPA);
     
 
     return 0;
