a2dd13065efe4b91f983fe3cf781f5c6cbd1e615
A-Tech-Services <abiodunoluwapelumi859@gmail.com>
2022-11-28 19:56:59 +0100

    update

--- HelloWorld.c
+++ HelloWorld.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <math.h>
 
 int main(){
     printf("Hello World!\n");
@@ -10,24 +11,125 @@ int main(){
     printf("\"I like pizza\" - Abraham Lincoln Probably\n");
 
     // Declaring a variable and assigning values
-    int age; // variable declared
-    age = 2; // variable initialized
-    float GP;
+     // variable declared
+    // age = 2; // variable initialized
+    // float GP;
 
-    GP = 3.07;
+    // GP = 3.07;
 
-    char grade = 'C';
+    // char grade = 'C';
 
-    char name[] = "Oluwapelumi";
+    // char name[] = "Oluwapelumi";
 
-    printf("%s\n", name);
-    printf("You are %d Years Old\n", age);
-    printf("Your GP is %0.15f\n", GP);
-    printf("Your Grade is %c\n", grade);
+    // printf("%s\n", name);
+    // printf("You are %d Years Old\n", age);
+    // printf("Your GP is %0.2f\n", GP);
+    // printf("Your Grade is %c\n", grade);
 
-    float CGPA = 2.354;
+    // float CGPA = 2.354;
 
-    printf("your CGPA is: %0.3f", CGPA);
+    // printf("your CGPA is: %0.3f", CGPA);
+
+    // const float PI = 3.14159;
+
+    // printf("\n%.3f", PI);
+
+    // int x = 5;
+    // int y = 2;
+
+    // float z = x / (float)y;
+
+    // char name[50];
+
+    // printf("%f", z);
+    // printf("What is your name?");
+    // scanf("%s", name);
+    // printf("You are %s\n", name);
+
+    // printf("How old are you?");
+    // scanf("%d", &age);
+    // printf("%d", age);
+
+    // char firstname[50];
+
+    // printf("What is your first name?: ");
+    // fgets(firstname, 50, stdin);
+    // printf("%s", firstname);
+    
+    // const float pi = 3.14159;
+
+    // float x = 5;
+    // float y = 2;
+
+    // float z = x/y;
+
+
+    // char fullname[100];
+    // int age;
+
+    // printf("Type in your full-Name: ");
+    // fgets(fullname, 100, stdin);
+
+    // printf("How old are you? ");
+    // scanf("%d", &age);
+
+    // printf("Your Full Name is: %s\n", fullname);
+    // printf("You are %d years old\n", age);
+
+    // int a = sqrt(9);
+    // printf("%d\n", a);
+
+    // int b = pow(2, 4);
+    // printf("%d", b);
+
+    // Program to solve for the circumference of a circle
+
+    // const double PI = 3.14159;
+    // double radius;
+    // double circum;
+
+    // printf("Enter the value for Radius: ");
+    // scanf("%lf", &radius);
+    
+    // circum = 2 * PI * radius;
+
+    // printf("The Circumference of the given circle = %.2lf", circum);
+
+
+    // Finding the hypoteneus of a right hand triangle.
+
+    // double a;
+    // double b;
+    // double c;
+
+    // printf("Enter the value for a: ");
+    // scanf("%lf", &a);
+
+    // printf("Enter the value for b: ");
+    // scanf("%lf", &b);
+
+
+    // c = sqrt(a*a + b*b);
+
+    // printf("Square root of a and b = %lf", c);
+
+    // int age;
+
+    // printf("Hello, what is your age? ");
+    // scanf("%d", &age);
+    
+    // switch(age){
+    //     case 12:
+    //         printf("your still a teenegaer");
+    //     case 18:
+    //         printf("Welcome to adult hood.");
+    //     default:
+    //         break;
+        
+    // }
+    
+
+
     
 
     return 0;
